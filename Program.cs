882de8989bdca9a1705aa92cea084764dcac4b94
Liezar <Edvin.salminen@gmail.com>
2022-10-31 11:02:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace The_Fountain_of_objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
